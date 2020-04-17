b72dc4636db7223c777f1f541a6dd0e26edbff4e
dad <60132399+dad2vad@users.noreply.github.com>
2020-04-17 07:32:33 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# qa
-Created with CodeSandbox
---
+++ config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "AIzaSyCWHV-59Nzhr050jL_Uk0kDQU56hmYRLS8",
+  authDomain: "vsamaru.firebaseapp.com",
+  databaseURL: "https://vsamaru.firebaseio.com",
+  projectId: "vsamaru",
+  storageBucket: "vsamaru.appspot.com",
+  messagingSenderId: "685290667281",
+  appId: "1:685290667281:web:469d5dd61070d5404460b0",
+  measurementId: "G-86X0K1BHN5"
+};
